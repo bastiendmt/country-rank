c1a5ae0733c30cb430f12f8bd4a868d3abeb2d52
bastienxs <bastienxs@gmail.com>
2020-11-12 01:08:57 +0100

    add random country button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,59 @@
-import Head from 'next/head'
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 import Layout from '../components/Layout/Layout'
 import SearchInput from '../components/SearchInput/SearchInput'
 import CountriesTables from '../components/CountriesTable/CountryTable'
 import styles from '../styles/Home.module.css'
+import { ShuffleRounded } from '@material-ui/icons'
+
 
 export default function Home({ countries }) {
-  const [keyword, setKeyword] = useState("")
+    const [keyword, setKeyword] = useState("")
+    const router = useRouter()
 
-  const filteredCountry = countries.filter(country =>
-    country.name.toLowerCase().includes(keyword) ||
-    country.region.toLowerCase().includes(keyword) ||
-    country.subregion.toLowerCase().includes(keyword)
-  )
+    const filteredCountry = countries.filter(country =>
+        country.name.toLowerCase().includes(keyword) ||
+        country.region.toLowerCase().includes(keyword) ||
+        country.subregion.toLowerCase().includes(keyword)
+    )
 
-  const onInputChange = (e) => {
-    e.preventDefault()
-    setKeyword(e.target.value.toLowerCase())
-  }
+    const onInputChange = (e) => {
+        e.preventDefault()
+        setKeyword(e.target.value.toLowerCase())
+    }
 
-  return <Layout>
-    <div className={styles.input_container}>
-      <div className={styles.counts}>Found {countries.length} countries</div>
+    const randomCountry = () => {
+        const random = Math.floor(Math.random() * filteredCountry.length) + 1
 
-      <div className={styles.input}>
-        <SearchInput placeholder="Filter by name, Region or SubRegion" onChange={onInputChange} />
-      </div>
-    </div>
+        return router.push(`/country/${countries[random].alpha3Code}`)
+    }
 
-    <CountriesTables countries={filteredCountry} />
-  </Layout>
+    return <Layout>
+        <div className={styles.input_container}>
+            <div className={styles.counts}>
+                <div>Found {countries.length} countries</div>
+
+                <button className={styles.shufflebutton} title="Random country" onClick={randomCountry}>
+                    <ShuffleRounded color='inherit' />
+                </button>
+            </div>
+
+            <div className={styles.input}>
+                <SearchInput placeholder="Filter by name, Region or SubRegion" onChange={onInputChange} />
+            </div>
+        </div>
+
+        <CountriesTables countries={filteredCountry} />
+    </Layout>
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch("https://restcountries.eu/rest/v2/all")
-  const countries = await res.json()
+    const res = await fetch("https://restcountries.eu/rest/v2/all")
+    const countries = await res.json()
 
-  return {
-    props: {
-      countries
+    return {
+        props: {
+            countries
+        }
     }
-  }
 }
