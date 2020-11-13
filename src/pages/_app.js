d6061a32831a836f1d038ff8d174a935d4a926b2
bastienxs <bastienxs@gmail.com>
2020-11-13 16:46:52 +0100

    format

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,26 +1,26 @@
-import React, {useState, useCallback, useMemo} from 'react'
+import React, { useState, useCallback, useMemo } from 'react'
 import '../styles/globals.css'
 
 export const LangContext = React.createContext({
-  lang : 'en',
-  switchLanguage : () => {  }
+  lang: 'en',
+  switchLanguage: () => { }
 })
 
 function MyApp({ Component, pageProps }) {
   const [lang, setLang] = useState('en')
 
-  const switchLanguage = useCallback(function() {
+  const switchLanguage = useCallback(function () {
     setLang(l => l === 'en' ? 'fr' : 'en')
   }, [])
 
-  const value = useMemo(function() {
+  const value = useMemo(function () {
     return {
       lang,
       switchLanguage
     }
-  },[lang, switchLanguage])
+  }, [lang, switchLanguage])
 
-  
+
   return <LangContext.Provider value={value}>
     <Component {...pageProps} />
   </LangContext.Provider>
