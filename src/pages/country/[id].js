cc8fdbbda929542443da5838642a63dcafb488e5
bastienxs <bastienxs@gmail.com>
2021-04-14 21:00:51 +0200

    fix double call to neighbors

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -20,14 +20,11 @@ const Country = ({ country }) => {
     const { lang } = useContext(LangContext)
 
     const getBorders = async () => {
+        console.log('GETTING NEIGHBORS')
         const borders = await Promise.all(country.borders.map(border => getCountry(border)))
 
         setBorders(borders)
     }
-
-    useEffect(() => {
-        getBorders()
-    }, [country])
 
     useEffect(() => {
         getBorders()
