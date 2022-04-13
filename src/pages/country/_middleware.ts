d2635376450c49a273d4a57c6a68f5cdede8236c
Bastien Dumont <bastienxs@gmail.com>
2022-04-13 21:39:21 +0200

    random country route

---
+++ src/pages/country/_middleware.ts
@@ -0,0 +1,21 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { API_URL } from "../../config";
+import { Countries } from "../../types/types";
+
+const randomCountry = (countries: Countries) => {
+  const random = Math.floor(Math.random() * countries.length);
+
+  return `/country/${countries[random].cca3}`;
+};
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const { pathname } = req.nextUrl;
+  if (pathname == "/country/random") {
+    const res = await fetch(`${API_URL}/all`);
+    const countries: Countries = await res.json();
+
+    return NextResponse.redirect(randomCountry(countries));
+  }
+
+  return NextResponse.next();
+}
