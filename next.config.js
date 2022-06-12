338f831a10d52c8f3cfae5bda082dcc0ddb0269a
Bastien Dumont <bastienxs@gmail.com>
2022-06-12 13:06:01 +0200

    fix pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
-// @ts-check
+/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-/**
- * @type {import('next').NextConfig}
- **/
-const nextConfig = {
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
   images: {
     domains: ["restcountries.com", "upload.wikimedia.org", "flagcdn.com"],
   },
   swcMinify: true,
-};
-
-module.exports = nextConfig;
+});
