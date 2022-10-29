be3c3c0b347f272e93e70cc4b52e467c6b4c820d
Bastien DUMONT <bastienxs@gmail.com>
2022-10-29 12:49:44 +0200

    nextjs 13

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  runtimeCaching,
+});
 
 module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    runtimeCaching,
-  },
   images: {
     domains: ["restcountries.com", "upload.wikimedia.org", "flagcdn.com"],
   },
-  swcMinify: true,
+  experimental: { appDir: true },
 });
