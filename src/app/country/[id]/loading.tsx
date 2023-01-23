f2e4fb87dc88f0c99644f11d2d887e6c2b7a72e4
Bastien DUMONT <bastienxs@gmail.com>
2023-01-23 22:57:37 +0100

    load country

---
+++ src/app/country/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import Layout from '../../../components/Layout/Layout';
+
+const Loading = () => (
+  <Layout title="Country rank">
+    <div>Loading country...</div>
+  </Layout>
+);
+
+export default Loading;
