136503dbbe87792ef30845d7270f552e76306cfc
Bastien DUMONT <bastienxs@gmail.com>
2023-01-30 11:42:02 +0100

    link to homepage

--- src/app/country/[id]/not-found.tsx
+++ src/app/country/[id]/not-found.tsx
@@ -1,3 +1,9 @@
-const NotFound = () => <div>Country not found</div>;
+import Link from 'next/link';
+
+const NotFound = () => (
+  <div>
+    Country not found, <Link href="/">go to home page</Link>
+  </div>
+);
 
 export default NotFound;
