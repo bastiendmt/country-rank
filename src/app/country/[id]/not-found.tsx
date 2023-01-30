6c36aeff7f0446e3b2079a3b0fbfeb71d7e72022
Bastien DUMONT <bastienxs@gmail.com>
2023-01-30 11:36:16 +0100

    return country not found

---
+++ src/app/country/[id]/not-found.tsx
@@ -0,0 +1,3 @@
+const NotFound = () => <div>Country not found</div>;
+
+export default NotFound;
--- src/app/country/[id]/page.tsx
+++ src/app/country/[id]/page.tsx
@@ -1,19 +1,18 @@
 import CountryDetails from '../../../components/CountryDetails/CountryDetails';
 import { API_URL } from '../../../config';
 import { Countries, Country } from '../../../types/types';
+import NotFound from './not-found';
 
-export async function generateStaticParams() {
+export async function generateStaticParams(): Promise<{ id: string }[]> {
   const res = await fetch(`${API_URL}/all`);
   const countries: Countries = await res.json();
 
-  const paths = countries.map((country) => ({
+  return countries.map((country) => ({
     id: country.cca3,
   }));
-
-  return paths;
 }
 
-async function getCountry(alphaCode: string) {
+async function getCountry(alphaCode: string): Promise<Country | undefined> {
   try {
     const res = await fetch(`${API_URL}/alpha/${alphaCode}`, {
       cache: 'force-cache',
@@ -21,16 +20,16 @@ async function getCountry(alphaCode: string) {
     if (!res.ok) {
       throw new Error(`Failed to fetch country: ${alphaCode}`);
     }
-    return res.json();
+    const countryData: [Country] = await res.json();
+    return countryData[0];
   } catch (error) {
-    console.log(error);
+    return undefined;
   }
-  return {};
 }
 
 const Country = async ({ params: { id } }: { params: { id: string } }) => {
-  const country: Country = (await getCountry(id))[0];
-
+  const country = await getCountry(id);
+  if (!country) return NotFound();
   return <CountryDetails country={country} />;
 };
 
