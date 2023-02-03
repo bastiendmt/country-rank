bfd064b8f62e04be6d757a4d95f08fe1b03fc428
Bastien DUMONT <bastienxs@gmail.com>
2023-02-03 20:23:02 +0100

    rework middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 import { API_URL } from './config';
 import { Countries } from './types/types';
 
-const randomCountry = (countries: Countries) => {
-  const random = Math.floor(Math.random() * countries.length);
-  return `/country/${countries[random].cca3}`;
-};
-
 export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
+  const { pathname, origin } = req.nextUrl;
   if (pathname === '/country/random') {
     const res = await fetch(`${API_URL}/all`);
     const countries: Countries = await res.json();
-
-    return NextResponse.redirect(randomCountry(countries));
+    const random = Math.floor(Math.random() * countries.length);
+    return NextResponse.rewrite(`${origin}/country/${countries[random].cca3}`);
   }
   return NextResponse.next();
 }
