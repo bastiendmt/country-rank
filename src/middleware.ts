6444c781a57c1ee473784b3bc174f2e3f43d054f
Bastien DUMONT <bastienxs@gmail.com>
2023-02-03 20:26:07 +0100

    use api functions

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { API_URL } from './config';
-import { Countries } from './types/types';
+import { getCountries } from './api/getCountries';
+import NotFound from './app/not-found';
 
 export async function middleware(req: NextRequest) {
   const { pathname, origin } = req.nextUrl;
   if (pathname === '/country/random') {
-    const res = await fetch(`${API_URL}/all`);
-    const countries: Countries = await res.json();
+    const countries = await getCountries();
+    if (!countries) return NotFound();
     const random = Math.floor(Math.random() * countries.length);
     return NextResponse.rewrite(`${origin}/country/${countries[random].cca3}`);
   }
