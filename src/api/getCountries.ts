75d112f2ff0b9de9c629cff765f53f5e71fc075a
Bastien Dumont <32489032+bastiendmt@users.noreply.github.com>
2023-02-04 01:04:15 +0100

    117 add jsdoc (#119)
    * add doc
    
    * export getBorders api

--- src/api/getCountries.ts
+++ src/api/getCountries.ts
@@ -1,6 +1,10 @@
 import { API_URL } from '@/config';
 import { Countries } from '@/types/types';
 
+/**
+ * Get countries from https://restcountries.com/v3.1/all
+ * @returns Country[]
+ */
 export async function getCountries(): Promise<Countries | undefined> {
   const res = await fetch(`${API_URL}/all`);
   if (!res.ok) {
--- src/api/getCountry.ts
+++ src/api/getCountry.ts
@@ -1,6 +1,13 @@
 import { API_URL } from '@/config';
 import { Country } from '@/types/types';
 
+/**
+ * Get countries from https://restcountries.com/v3.1/alpha/[alphaCode]
+ * @param {string} alphaCode - 3 character alpha code of the desired country
+ * @example
+ * // https://restcountries.com/v3.1/alpha/fra
+ * @returns Country[]
+ */
 export async function getCountry(
   alphaCode: string,
 ): Promise<Country | undefined> {
