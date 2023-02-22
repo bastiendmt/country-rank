70bef919fe1acecaaba6d5dd66232118464de490
Bastien DUMONT <bastienxs@gmail.com>
2023-02-22 21:22:50 +0100

    fix: hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import styles from '@/styles/layout.module.css';
 import translationsContent from '@/translations/translations';
 import { TranslationType } from '@/types/types';
 import { Brightness6Rounded, LanguageRounded } from '@mui/icons-material';
-import Head from 'next/head';
 import Link from 'next/link';
 import { useContext, useState } from 'react';
 import { LangContext } from './_app';
@@ -33,28 +32,14 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <html lang="fr">
-      <Head>
-        <title>Country rank</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta charSet="UTF-8" />
-        <meta name="application-name" content="Country rank" />
-        <meta name="apple-mobile-web-app-title" content="Country rank" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="theme-color" content="#252329" />
-        <meta name="description" content="Country ranking" />
-        <meta name="keywords" content="rank, country, infos, stats" />
-        <meta name="author" content="Bastien Dumont" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/apple-icon.png" />
-      </Head>
       <body>
         <div className={styles.container}>
           <header className={styles.header}>
-            <Link href="/" passHref>
-              <Logo />
-            </Link>
+            <div>
+              <Link href="/" passHref>
+                <Logo />
+              </Link>
+            </div>
             <button
               type="button"
               className={styles.theme_switcher}
