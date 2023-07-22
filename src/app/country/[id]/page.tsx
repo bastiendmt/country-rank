fc2de5042d4e1c052607172f156af803c0220c2b
Bastien Dumont <bastienxs@gmail.com>
2023-07-22 20:28:10 +0200

    move outside layout

--- src/app/country/[id]/page.tsx
+++ src/app/country/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getCountry } from '@/api/getCountry';
+import { OG_DESCRIPTION, OG_TITLE } from '@/app/og';
 import CountryDetails from '@/components/CountryDetails/CountryDetails';
 import { API_URL } from '@/config';
 import { Countries } from '@/types/types';
@@ -22,12 +23,11 @@ export async function generateMetadata({
 }: PageProps): Promise<Metadata> {
   const country = await getCountry(id);
   return {
-    title: country?.name.common
-      ? `Country rank - ${country.name.common}`
-      : 'Country rank - Country page',
+    description: `Find more about ${country.name.common} by clicking on this link. ${OG_DESCRIPTION}`,
     openGraph: {
       images: [country.flags.png],
     },
+    title: `${OG_TITLE} - ${country.name.common}`,
   };
 }
 
