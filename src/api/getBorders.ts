109321bd2f122d9897e21fc3630aee54b3ffc2a4
Bastien Dumont <bastienxs@gmail.com>
2023-08-18 11:01:21 +0200

    api & layout

--- src/api/getBorders.ts
+++ src/api/getBorders.ts
@@ -1,4 +1,4 @@
-import { API_URL } from '@/config';
+import { API_URL } from '@/api-url';
 import { Country } from '@/types/types';
 
 /**
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { HeaderButtons } from '@/components/HeaderButtons';
 import { Logo } from '@/logo';
-import styles from '@/styles/layout.module.css';
+import styles from './layout.module.css';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang="fr">
