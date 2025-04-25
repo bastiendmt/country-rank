e3946720638fb8f74d21129b2f19d4fc61994c3a
Bastien DUMONT <bastienxs@gmail.com>
2025-04-26 00:30:23 +0200

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,49 +1,42 @@
 import { type NextRequest, NextResponse } from 'next/server';
 import { getCountries } from './api/getCountries';
 import NotFound from './app/[lang]/not-found';
+import { i18n } from 'i18n-config';
 
-// export async function middleware(req: NextRequest) {
-//   const { pathname, origin } = req.nextUrl;
-//   if (pathname === '/country/random') {
-//     const countries = await getCountries();
-//     if (!countries) return NotFound();
-//     const randomIndex = Math.floor(Math.random() * countries.length);
-//     const countryCode = countries[randomIndex]?.cca3;
-//     if (!countryCode) return NextResponse.next();
-//     // rewriting the url doesn't seen to work
-//     return NextResponse.rewrite(`${origin}/country/${countryCode}`);
-//   }
-//   return NextResponse.next();
-// }
+const { defaultLocale, locales } = i18n;
 
-// export const config = {
-//   matcher: ['/country/:country*'],
-// };
+export async function middleware(request: NextRequest) {
+  const { pathname, origin } = request.nextUrl;
 
-const locales = ['en', 'fr'];
-
-export function middleware(request: NextRequest) {
-  // Check if there is any supported locale in the pathname
-  const { pathname } = request.nextUrl;
-  const pathnameHasLocale = locales.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  const localeCountryRandom = locales.find(
+    (loc) => pathname === `/${loc}/country/random`,
   );
+  const locale = locales.find(
+    (loc) => pathname === `/${loc}` || pathname.startsWith(`/${loc}/`),
+  );
+  // Handle random country
+  if (localeCountryRandom) {
+    const countries = await getCountries();
+    if (!countries) return NotFound();
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    const countryCode = countries[randomIndex]?.cca3;
+    if (!countryCode) return NextResponse.next();
+    return NextResponse.redirect(`${origin}/${locale}/country/${countryCode}`);
+  }
 
-  if (pathnameHasLocale) return;
+  // Allow if path is exactly "/{locale}" or starts with "/{locale}/"
+  if (locale) {
+    return NextResponse.next();
+  }
 
-  // Redirect if there is no locale
-  const locale = 'fr'; // Default locale
-  request.nextUrl.pathname = `/${locale}${pathname}`;
-  // e.g. incoming request is /products
-  // The new URL is now /en-US/products
-  return NextResponse.redirect(request.nextUrl);
+  // Redirect to default locale if no locale is found keeping the pathname
+  return NextResponse.redirect(
+    new URL(`/${defaultLocale}${pathname}`, request.url),
+  );
 }
 
 export const config = {
   matcher: [
-    // Skip all internal paths (_next)
-    '/((?!_next).*)',
-    // Optional: only run on root (/) URL
-    // '/'
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|sw.js).*)',
   ],
 };
