887a2c01323641060b5adebe8729ce24246a2cbb
Bastien DUMONT <bastienxs@gmail.com>
2025-04-26 12:47:47 +0200

    translate error page

--- src/app/[lang]/country/[id]/not-found.tsx
+++ src/app/[lang]/country/[id]/not-found.tsx
@@ -1,10 +1,12 @@
 import { LinkHome } from '@/components/link-home';
+import { getDictionary } from '../../dictionaries';
+import { Locale } from 'i18n-config';
 
-const NotFound = () => {
+const NotFound = async ({ lang }: { lang: Locale }) => {
+  const dict = await getDictionary(lang);
   return (
     <div>
-      An error has occurred, please try again later or{' '}
-      <LinkHome>go to home page</LinkHome>
+      {dict.error.generic} <LinkHome>{dict.error.linkHome}</LinkHome>
     </div>
   );
 };
