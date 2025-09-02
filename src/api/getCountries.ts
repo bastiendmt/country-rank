7879e91c604921308033dd98cc60859a202c460f
Bastien DUMONT <bastienxs@gmail.com>
2025-09-02 18:26:46 +0200

    Fix api usage

--- src/api/getCountries.ts
+++ src/api/getCountries.ts
@@ -2,11 +2,13 @@ import { API_URL } from '@/api/url';
 import { Countries } from '@/types';
 
 /**
- * Get countries from https://restcountries.com/v3.1/all
+ * Get countries from https://restcountries.com/v3.1/all?fields=name,flags,cca3
  * @returns Country[]
  */
 export async function getCountries(): Promise<Countries | undefined> {
-  const res = await fetch(`${API_URL}/all`);
+  const res = await fetch(
+    `${API_URL}/all?fields=name,flags,area,gini,population,translations,cca3`,
+  );
   if (!res.ok) {
     throw new Error('Failed to fetch countries');
   }
