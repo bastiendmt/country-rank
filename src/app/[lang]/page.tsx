bf58930760121afe07f96b6fff996ba2e9e8903c
Bastien Dumont <bastienxs@gmail.com>
2026-04-13 09:24:10 +0200

    static params

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
+import { Suspense } from 'react';
 import { getCountries } from '@/api/getCountries';
 import CountriesTable from '@/components/CountriesTable/CountriesTable';
+import { i18n } from 'i18n-config';
 import NotFound from './not-found';
 import { OG_DESCRIPTION, OG_TITLE } from './og';
 import { getDictionary } from './dictionaries';
@@ -10,13 +12,21 @@ export const metadata: Metadata = {
   description: OG_DESCRIPTION,
 };
 
+export function generateStaticParams() {
+  return i18n.locales.map((locale) => ({ lang: locale }));
+}
+
 // eslint-disable-next-line no-undef
 const Index = async ({ params }: PageProps<'/[lang]'>) => {
   const { lang } = await params;
   const countries = await getCountries();
   if (!countries) return NotFound();
   const dict = await getDictionary(lang as 'en' | 'fr');
-  return <CountriesTable countries={countries} dictionary={dict} />;
+  return (
+    <Suspense>
+      <CountriesTable countries={countries} dictionary={dict} />
+    </Suspense>
+  );
 };
 
 export default Index;
